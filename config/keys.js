188df0c1a2a035d1e878f7c5d007bdb83b66c901
Shubham Sharma <47448459+sksharmajsr@users.noreply.github.com>
2019-07-04 22:28:24 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
 module.exports = {
-    mongoURI: "mongodb+srv://user:G4Qk20JV6i7ku5Q3@itsxperthub-slhqz.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "Your Mongo URI here",
     secretOrKey: "secret"
+    
+    //Mail Configuration
+    host: "smtp.gmail.com", //for gmail user
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    user: "Email Username",//gmail id for gmail user
+    pass: "Email Password"//gmail password for gmail user
   };
